b95c41d66969dc227e595b1949053684ad37db79
Michael E. Shea <mike@mikeshea.net>
2025-06-30 17:52:50 -0400

    Removed redundant a5e files. Updated search index.

---
+++ 5e_artisanal_database/search/filter-data.js
@@ -0,0 +1,93 @@
+// Auto-generated source metadata for 5e Artisanal Database
+// Generated at: 2025-06-30T21:51:47.425Z
+
+window.sourceMetadata = [
+  {
+    "type": "ruleset",
+    "source": "5.1 SRD (D&D 2014)"
+  },
+  {
+    "type": "monsters",
+    "category": "5.1_srd_(2015_mm)"
+  },
+  {
+    "type": "magic_items",
+    "category": "5.1_srd_(d&d_2014)"
+  },
+  {
+    "type": "spells",
+    "category": "5.1_srd_(d&d_2014)"
+  },
+  {
+    "type": "ruleset",
+    "source": "5.2 SRD (D&D 2024)"
+  },
+  {
+    "type": "monsters",
+    "category": "5.2_srd_(2025_mm)"
+  },
+  {
+    "type": "magic_items",
+    "category": "5.2_srd_(d&d_2024)"
+  },
+  {
+    "type": "spells",
+    "category": "5.2_srd_(d&d_2024)"
+  },
+  {
+    "type": "monsters",
+    "category": "a5e_monstrous_menagerie"
+  },
+  {
+    "type": "ruleset",
+    "source": "Black Flag (Tales of the Valiant)"
+  },
+  {
+    "type": "monsters",
+    "category": "black_flag"
+  },
+  {
+    "type": "monsters",
+    "category": "creature_codex"
+  },
+  {
+    "type": "spells",
+    "category": "deep_magic_5e"
+  },
+  {
+    "type": "ruleset",
+    "source": "Lazy GM's Monster Resource Document"
+  },
+  {
+    "type": "ruleset",
+    "source": "Lazy GM's Resource Document"
+  },
+  {
+    "type": "ruleset",
+    "source": "Level Up Advanced 5e"
+  },
+  {
+    "type": "magic_items",
+    "category": "level_up_advanced_5e"
+  },
+  {
+    "type": "spells",
+    "category": "level_up_advanced_5e"
+  },
+  {
+    "type": "monsters",
+    "category": "tome_of_beasts_2"
+  },
+  {
+    "type": "monsters",
+    "category": "tome_of_beasts_2023"
+  },
+  {
+    "type": "monsters",
+    "category": "tome_of_beasts_3"
+  },
+  {
+    "type": "magic_items",
+    "category": "vault_of_magic"
+  }
+];
